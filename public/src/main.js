ab388eda6d3def94ad6f881d0537b61ea2a6e6c1
ckpiggy <mrgmp2004@hotmail.com>
2017-04-30 23:56:17 +0800

    remove test request

--- public/src/main.js
+++ public/src/main.js
@@ -5,13 +5,3 @@ new Vue({
   el: '#app',
   render: h => h(App)
 })
-
-var xhttp = new XMLHttpRequest()
-xhttp.onreadystatechange = function () {
-  if(xhttp.readyState == XMLHttpRequest.DONE && xhttp.status == 200){
-    console.log(xhttp.responseText)
-    console.log('test for watch~~')
-  }
-}
-xhttp.open('GET', '/api', true)
-xhttp.send()
